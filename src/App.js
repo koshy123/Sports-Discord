0bca600c5ce50a00ba551ccd979948b77e877881
humza15 <humzazafarullah@gmail.com>
2022-12-08 14:54:43 -0500

    getting data from backend

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import Home from "./Home"
 import {Route, Routes} from 'react-router-dom'
 import Nav from "./Nav"
 import About from "./About"
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState }  from 'react';
+import axios from 'axios';
+
 
 function App() {
-  const {topic, Topic} = useState
-  const {topics, useTopic} = useState    
-
+  const [loading, setLoading] = useState(false)
+  const [topics, setTopics] = useState([])
   
-
+  const getTopics = {
+    method: 'GET',
+    url: 'https://sports-discord.fly.dev/api/topics',
+  };
   
-  
+  useEffect(() => {
+    axios.request(getTopics).then(function (response) {
+    setTopics(response.data);
+    console.log(response.data)
+    setLoading(true);
+  }).catch(function (error) {
+     console.error(error);
+  })
+  }, [])
   
   
   return (
-     <div className="App">
+    <div className="App">
       <Nav />
       <main>
         <Routes>
@@ -27,5 +39,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
