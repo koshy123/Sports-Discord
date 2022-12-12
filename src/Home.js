b7324f2ab527ba403d5287569ecfb595e4e08573
maxbond <ceo@rf.ru>
2022-12-12 22:26:18 +0300

    responsive design

--- src/Home.js
+++ src/Home.js
@@ -21,6 +21,7 @@ function Home({ topics, setTopics }) {
     <>
       <div className="title">
         <img
+          className="mainPhoto"
           // className="square"
           // width="1400"
           height="400"
