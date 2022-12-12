854a00eeffaabce0a4c0e624c538c91ce8f0ea03
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-12-12 15:36:16 -0500

    bottom footer

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
   return (
     <div className="App" style={{ maxWidth: 1000, margin: "0 auto" }}>
       <Nav />
-      <main>
+      <main className="mainPage">
         <Routes>
           <Route
             path="/"
